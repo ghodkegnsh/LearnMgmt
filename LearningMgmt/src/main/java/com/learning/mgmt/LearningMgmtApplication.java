6e1924d644654f7e640c0f1d9fcfa4877c37e743
91800 <91800@GANESH-DESKTOP>
2021-02-17 15:19:41 +0530

    Master Project structure

---
+++ LearningMgmt/src/main/java/com/learning/mgmt/LearningMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.learning.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningMgmtApplication.class, args);
+	}
+
+}
