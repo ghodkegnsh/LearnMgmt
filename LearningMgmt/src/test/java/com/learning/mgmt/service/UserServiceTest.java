ac4f5b3f4722ab4ee789264d471e7591fca08032
91800 <91800@GANESH-DESKTOP>
2021-02-18 13:02:36 +0530

    Test cases fix and code coverage fix

--- LearningMgmt/src/test/java/com/learning/mgmt/service/UserServiceTest.java
+++ LearningMgmt/src/test/java/com/learning/mgmt/service/UserServiceTest.java
@@ -15,87 +15,83 @@ import com.learning.mgmt.entity.User;
 import com.learning.mgmt.repository.UserRepository;
 
 @SpringBootTest
-public class UserServiceTest {
+class UserServiceTest {
 
 	@InjectMocks
 	UserService userService;
-	
+
 	@Mock
 	UserRepository userRepository;
-	
+
 	@Mock
 	User user;
-	
+
 	@Test
-	public void testRegisterUserSuccess() {
-		String result=userService.registerUser(user);
-		assertEquals("User Registered Successfully!", result);	
+	void testRegisterUserSuccess() {
+		String result = userService.registerUser(user);
+		assertEquals("User Registered Successfully!", result);
 	}
-	
+
 	@Test
-	public void testRegisterUserforAlreadyRegistered() {
-		String emailId="abc@gmail.com";
+	void testRegisterUserforAlreadyRegistered() {
+		String emailId = "abc@gmail.com";
 		when(user.getEmailId()).thenReturn(emailId);
-		User user=mock(User.class);
 		when(userRepository.findByEmailId(emailId)).thenReturn(user);
-		String result=userService.registerUser(user);
+		String result = userService.registerUser(user);
 		assertEquals("User is already registered!", result);
 	}
-	
+
 	@Test
-	public void testRegisterUserforUserNameCheck () {
-		List<User> userList=List.of(mock(User.class));
+	void testRegisterUserforUserNameCheck() {
+		List<User> userList = List.of(mock(User.class));
 		when(userList.get(0).getUserName()).thenReturn("userName");
 		when(user.getUserName()).thenReturn("userName");
 		when(userRepository.findAll()).thenReturn(userList);
-		String result=userService.registerUser(user);
+		String result = userService.registerUser(user);
 		assertEquals("User name is already taken!", result);
 	}
-	
+
 	@Test
-	public void testRegisterUserforPasswordCheck () {
-		List<User> userList=List.of(mock(User.class));
+	void testRegisterUserforPasswordCheck() {
+		List<User> userList = List.of(mock(User.class));
 		when(userList.get(0).getUserName()).thenReturn("userName");
 		when(user.getUserName()).thenReturn("UserName");
 		when(userList.get(0).getPassword()).thenReturn("Password");
 		when(user.getPassword()).thenReturn("Password");
 		when(userRepository.findAll()).thenReturn(userList);
-		String result=userService.registerUser(user);
+		String result = userService.registerUser(user);
 		assertEquals("Password is already taken!", result);
 	}
-	
+
 	@Test
-	public void testUserLoginSuccess() {
-		String userName="userName";
-		String password="Password";
-		User user=mock(User.class);
+	void testUserLoginSuccess() {
+		String userName = "userName";
+		String password = "Password";
+		User user = mock(User.class);
 		when(userRepository.findByUserName(userName)).thenReturn(user);
 		when(user.getPassword()).thenReturn(password);
-		String result=userService.userLogin(userName, password);
+		String result = userService.userLogin(userName, password);
 		assertEquals("User Login Success!", result);
 	}
-	
+
 	@Test
-	public void testUserLoginforUserName() {
-		String userName="userName";
-		String password="Password";
+	void testUserLoginforUserName() {
+		String userName = "userName";
+		String password = "Password";
 		when(userRepository.findByUserName(userName)).thenReturn(null);
-		String result=userService.userLogin(userName, password);
+		String result = userService.userLogin(userName, password);
 		assertEquals("Check User Name Or User is not registered", result);
 	}
-	
+
 	@Test
-	public void testUserLoginFailure() {
-		String userName="userName";
-		String password="Password";
-		User user=mock(User.class);
+	void testUserLoginFailure() {
+		String userName = "userName";
+		String password = "Password";
+		User user = mock(User.class);
 		when(userRepository.findByUserName(userName)).thenReturn(user);
 		when(user.getPassword()).thenReturn("pass");
-		String result=userService.userLogin(userName, password);
+		String result = userService.userLogin(userName, password);
 		assertEquals("Check Password!", result);
 	}
-	
-	
-	
-	
+
 }
